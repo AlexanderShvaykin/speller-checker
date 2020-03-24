aa3163da040c7b3ea096f3bbdc097e5108baa6e1
Alexander Shvaykin <skiline.alex@gmail.com>
2020-03-24 23:54:17 +0300

    add test

---
+++ api/client_test.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	response := `OK!`
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// Send response to be tested
+		rw.Write([]byte(response))
+	}))
+
+	result := Get(server.URL)
+	if result != response {
+		t.Errorf("unexpected result: got %s, want %s", result, response)
+	}
+}
