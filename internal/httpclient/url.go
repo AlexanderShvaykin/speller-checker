73013fbc1ae30b88fb27128b94f2fe75e4d32e29
Alexander Shvaykin <skiline.alex@gmail.com>
2020-03-25 23:40:48 +0300

    fix url decode

--- internal/httpclient/url.go
+++ internal/httpclient/url.go
@@ -1,12 +1,14 @@
 package httpclient
 
+import "net/url"
+
 // BuildURL build url with base url and params
 func BuildURL(base string, p map[string]string) string {
 	base += "?"
 	for key, value := range p {
 		base += key
 		base += "="
-		base += value
+		base += url.QueryEscape(value)
 		base += "&"
 	}
 	return base[0:(len(base) - 1)]
